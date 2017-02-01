f302a79e89dfde190a562ef6ae41b5606e7346a6
HacKan <hackan@gmail.com>
2017-02-01 00:35:42 -0300

    Created extension, added man page, unit tests, makefile and readme

---
+++ README.md
@@ -0,0 +1,68 @@
+# pass insertfile 
+
+A [pass](https://www.passwordstore.org/) extension that provides a convenient solution to insert a file into the store.
+
+If you are using the [forked pass](https://github.com/HacKanCuBa/password-store), then you might prefer using [pass insert](https://github.com/HacKanCuBa/pass-extension-insert) extension.
+
+## Usage
+
+```
+Usage:
+    pass insertfile [--help,-h] [--force,-f] pass-name file-path
+            Insert a file into the store. Prompt before
+            overwriting existing password unless forced (--force,-f).
+
+More information may be found in the pass-insertfile(1) man page.
+```
+
+See `man pass-insertfile` for more information.
+
+## Example
+
+Insert your ssh private key.
+
+    pass insertfile Systems/General/SSHKey ~/.ssh/id_rsa
+
+## Installation
+
+### Linux
+
+    git clone https://git.linuxnoblog.net/hackan/pass-insertfile/
+    cd pass-insertfile
+    sudo make install
+
+Or simply copy *insertfile.bash* to the pass extension directory (~/.password-store/.extensions by default).
+
+#### Requirements
+
+In order to use extension with `pass`, you need:
+* `pass 1.7.0` or greater. As of today this version has not been released yet.
+Therefore you need to install it by hand from zx2c4.com:
+
+      git clone https://git.zx2c4.com/password-store
+      cd password-store
+      sudo make install
+
+* You need to enable the extensions in pass: `PASSWORD_STORE_ENABLE_EXTENSIONS=true pass`.
+You can create an alias in `.bashrc`: `alias pass='PASSWORD_STORE_ENABLE_EXTENSIONS=true pass'`
+
+## Contribution
+Feedback, contributors, pull requests are all very welcome.
+
+## License
+
+    Copyright (C) 2017 HacKan (https://hackan.net)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
---
+++ inserfile.bash
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+# pass insertfile - Password Store Extension (https://www.passwordstore.org/)
+# Copyright (C) 2017 HacKan <hackan@gmail.com>.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+cmd_insertfile_usage() {
+	cat <<-_EOF
+	Usage:
+	    $PROGRAM insertfile [--help,-h] [--force,-f] pass-name file-path
+	        Insert a file into the store. Prompt before
+	        overwriting existing password unless forced (--force,-f). 
+	        
+	More information may be found in the pass-insertfile(1) man page.	        
+	_EOF
+}
+
+cmd_insertfile() {
+	local opts force=0
+	opts="$($GETOPT -o f -l force -n "$PROGRAM $COMMAND" -- "$@")"
+	local err=$?
+	eval set -- "$opts"
+	while true; do case $1 in
+		-f|--force) force=1; shift ;;
+		--) shift; break ;;
+	esac done
+	
+	[[ $err -ne 0 || $# -ne 2 ]] && die "Usage: $PROGRAM $COMMAND [--help,-h] [--force,-f] pass-name file-path"
+	
+    local path="${1%/}"
+	local passfile="$PREFIX/$path.gpg"
+	check_sneaky_paths "$path"
+
+	[[ $force -eq 0 && -e $passfile ]] && yesno "An entry already exists for $path. Overwrite it?"
+
+	mkdir -p -v "$PREFIX/$(dirname "$path")"
+	set_gpg_recipients "$(dirname "$path")"
+    
+    local filepath="${2}"
+    [[ -r "${filepath}" && -L "${filepath}" ]] && yesno "The file is a symlink and it's going to be resolved. Are you sure you want that?"
+
+    if [[ -r "${filepath}" && -f "${filepath}" ]]; then
+        check_sneaky_paths "${filepath}"
+        $GPG -e "${GPG_RECIPIENT_ARGS[@]}" -o "$passfile" "${GPG_OPTS[@]}" "${filepath}" || die "File encryption aborted."
+    else
+        die "File ${filepath} is not valid."
+    fi
+    
+    git_add_file "$passfile" "Add given file for $path to store."
+}
+
+[[ "$1" == "help" || "$1" == "--help" || "$1" == "-h" ]] && cmd_insertfile_usage && exit 0
+
+cmd_insertfile "$@"
